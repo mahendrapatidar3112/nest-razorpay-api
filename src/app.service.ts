a2939c3b00e9dd55b5b01bad01e4208682d03f5f
Mahendra_Patidar <mpatidar3112@gmail.com>
2025-09-29 12:48:13 +0530

    add dotenv and setup.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('Hello World, It Is A Razorpay Practice App.');
     return 'Hello World, It Is A Razorpay Practice App.';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const port = process.env.PORT;
+
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
+
 bootstrap();
